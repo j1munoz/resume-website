b809ed5743513638f499d95994297ab213ad7671
j1munoz <jimmymunoz397@gmail.com>
2025-07-25 00:28:52 -0700

    Husky install

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-resume-tan-100 font-resume-ubuntu antialiased">
+        
         {children}
       </body>
     </html>
