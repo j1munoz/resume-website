71051959da8fb9d173aa5279d8a7a07406a49fab
j1munoz <jimmymunoz397@gmail.com>
2025-09-05 16:21:05 -0700

    Who I Am component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,11 @@
+import WhoIAm from "@/components/about/whoiam";
+
 const About = () => {
-  return <div className="flex flex-col items-center justify-center">About</div>;
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <WhoIAm />
+    </div>
+  )
 };
 
 export default About;
