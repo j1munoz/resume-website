7bdeead59abc1209989fc737ea22084aab6432fb
j1munoz <jimmymunoz397@gmail.com>
2025-09-23 15:54:10 -0700

    Tech stack

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,11 @@
 import WhoIAm from "@/components/about/whoiam";
+import Tools from "@/components/about/tools";
 
 const About = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center gap-5">
       <WhoIAm />
+      <Tools />
     </div>
   );
 };
